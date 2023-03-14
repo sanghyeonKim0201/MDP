7514f424a8c283896169f4f1785171d86a72c85b
sanghyeonKim0201 <gmhty4345@gmail.com>
2023-03-14 16:43:54 +0900

    start

---
+++ MDPServer/src/main/java/com/example/MDPServer/MdpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MDPServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdpServerApplication.class, args);
+	}
+
+}
---
+++ MDPServer/src/test/java/com/example/MDPServer/MdpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MDPServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
