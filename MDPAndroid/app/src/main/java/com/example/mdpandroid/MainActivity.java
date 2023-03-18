c21bcc1c0b2a30d2d81e8325a4ed970f3aff257d
sanghyeonKim0201 <gmhty4345@gmail.com>
2023-03-18 15:30:07 +0900

    main 80%

--- MDPAndroid/app/src/main/java/com/example/mdpandroid/MainActivity.java
+++ MDPAndroid/app/src/main/java/com/example/mdpandroid/MainActivity.java
@@ -1,30 +1,39 @@
 package com.example.mdpandroid;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
+import java.time.LocalDate;
 import java.util.HashMap;
 
 import io.reactivex.rxjava3.core.Observable;
 
 public class MainActivity extends AppCompatActivity implements JsonDataToServer{
 
-    Button searchBtn;
-    Spinner com[] = new Spinner[2];
+    TextView searchBtn;
+    Spinner com[] = new Spinner[3];
+    String[] comKey = new String[3];
     EditText txt[] = new EditText[3];
     HashMap<String, String>airLine = new HashMap<>();
     HashMap<String, String>airPlane = new HashMap<>();
-
+    CheckBox chk;
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,16 +43,77 @@ public class MainActivity extends AppCompatActivity implements JsonDataToServer{
         data();
         loadAirLine();
         loadAirPlane();
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    com[2].setEnabled(false);
+                    comKey[2] = "NO";
+                }else{
+                    com[2].setEnabled(true);
+                }
+            }
+        });
+
+        for(Spinner c : com){
+            c.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    if(c.getId() == R.id.startCom){
+                        String[] str = airPlane.entrySet().stream().filter(a->a.getValue().indexOf(parent.getSelectedItem().toString()) == -1).map(a->a.getValue()).toArray(String[]::new);
+                        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_spinner_item, str);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        com[1].setAdapter(adapter);
+                        comKey[0] = airPlane.entrySet().stream().filter(a->a.getValue().indexOf(parent.getSelectedItem().toString()) != -1).map(a->a.getKey().toString()).findAny().get().toString();
+                    }else if(c.getId() == R.id.airLineCom){
+                        comKey[2] = airLine.entrySet().stream().filter(a->a.getValue().indexOf(parent.getSelectedItem().toString()) != -1).map(a->a.getKey().toString()).findAny().get().toString();
+                    }else{
+                        comKey[1] = airPlane.entrySet().stream().filter(a->a.getValue().indexOf(parent.getSelectedItem().toString()) != -1).map(a->a.getKey().toString()).findAny().get().toString();
+                    }
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        searchBtn.setOnClickListener(a->{
+            for(EditText t : txt){
+                if(t.getText().toString().equals("")){
+                    builder.setTitle("경고").setMessage("예약 일자를 선택해주세요").create().show();
+                    return;
+                }
+            }
+            try {
+                int txt1 = Integer.parseInt(txt[0].getText().toString());
+                int txt2 = Integer.parseInt(txt[1].getText().toString());
+                int txt3 = Integer.parseInt(txt[2].getText().toString());
+
+                LocalDate date = LocalDate.parse(String.format("%04d-%02d-%02d", txt1, txt2, txt3));
+                if(LocalDate.now().toEpochDay() > date.toEpochDay()){
+                    builder.setTitle("경고").setMessage("이미 지난 날은 예약 불가 합니다").create().show();
+                    return;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                builder.setTitle("경고").setMessage("날짜가 형식이 올바 르지 않습니다 (yyyyMMdd)").create().show();
+                return;
+            }
+            //값 보내기
+
+        });
     }
     void data(){
         for(int i = 0; i < txt.length; i++){
             txt[i] = findViewById(new int[]{R.id.yearTxt, R.id.monthTxt, R.id.dayTxt}[i]);
         }
         for(int i = 0; i < com.length; i++){
-            com[i] = findViewById(new int[]{R.id.startCom, R.id.arrCom}[i]);
-            com[i].setPrompt("출발지를 선택하세요, 도착지를 선택하세요".split(",")[i]);
+            com[i] = findViewById(new int[]{R.id.startCom, R.id.arrCom, R.id.airLineCom}[i]);
         }
-        searchBtn = findViewById(R.id.searchBtn);
+        searchBtn = findViewById(R.id.searchTxt);
+        chk = findViewById(R.id.selectChk);
+        builder = new AlertDialog.Builder(MainActivity.this);
     }
     void loadAirPlane(){
         try {
@@ -85,7 +155,7 @@ public class MainActivity extends AppCompatActivity implements JsonDataToServer{
                 JSONArray jsonArray = jsonObject.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
                 for(int i =0; i <  jsonArray.length(); i++){
                     JSONObject item = jsonArray.getJSONObject(i);
-                    airLine.put(item.getString("airlineNm"), item.getString("airlineNm"));
+                    airLine.put(item.getString("airlineId"), item.getString("airlineNm"));
                 }
                 ArrayAdapter<String> array = new ArrayAdapter<String>(MainActivity.this,
                         android.R.layout.simple_spinner_item,
@@ -93,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements JsonDataToServer{
                                 a.getValue()
                         ).toArray(String[]::new));
                 array.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                com[1].setAdapter(array);
+                com[2].setAdapter(array);
             });
         }catch (Exception e){
 
